7a1e495fb1d0453b000500b0727e8e4042290bc2
unknown <it20136710@my.sliit.lk>
2024-09-14 15:36:51 +0530

    complete to do application

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import { TodoProvider } from './context/TodoContext';
+import Home from './pages/Home';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+import Todos from './pages/Todos';
+import PrivateRoute from './components/PrivateRoute';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <TodoProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+      
+            <Route
+              path="/todos"
+              element={
+                <PrivateRoute>
+                  <Todos />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </Router>
+        <ToastContainer />
+      </TodoProvider>
+    </AuthProvider>
   );
 }
 
